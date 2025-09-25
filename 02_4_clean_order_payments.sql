3b80b5832167a25935ab0d33d97dd4c4ea2b569c
Chinonso Nwafor <bernardnwafor90@gmail.com>
2025-09-25 19:36:03 +0100

    Standardized and removed duplicates

--- 02_4_clean_order_payments.sql
+++ 02_4_clean_order_payments.sql
@@ -12,5 +12,43 @@ INTO clean.order_payments
 FROM staging.order_payments
 
 
+-- Null (dynamically creates sql string checking all the columns names of the order_payment for null)
+DECLARE @sql NVARCHAR(MAX);
+
+SELECT @sql = 
+	' SELECT * 
+	  FROM [clean].[order_payments] 
+	  WHERE ' + STRING_AGG('['+ COLUMN_NAME +'] IS NULL ', ' OR ')
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'order_payments' AND TABLE_SCHEMA = 'clean'
+EXEC sp_executesql @sql
+
+-- No Null
+
+
+-- Standardize
+SELECT order_id, payment_sequential, payment_type, payment_installments, payment_value
+FROM clean.order_payments
+-- No standardisation needed
+
+
+-- Duplicates (Check for duplicates with partition by)
+SELECT *
+FROM (
+		SELECT *, ROW_NUMBER () OVER (PARTITION BY order_id, payment_sequential, payment_type, payment_installments, payment_value 
+					ORDER BY (SELECT NULL)) AS occurance
+		FROM clean.order_payments) AS duplicate
+WHERE occurance > 1
+-- No duplicates found
+
+
+SELECT TOP (2) *
+FROM clean.order_payments
+
+
+
+
+
+
 
 
