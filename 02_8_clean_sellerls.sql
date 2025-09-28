1f1a5b3ec548ecb44d00bfb9e8993a0b0450ae7a
Chinonso Nwafor <bernardnwafor90@gmail.com>
2025-09-28 19:46:22 +0100

    Checked for null, add derived state full column, checked for duplicates

--- 02_8_clean_sellerls.sql
+++ 02_8_clean_sellerls.sql
@@ -14,11 +14,44 @@ SELECT
 INTO clean.sellers
 FROM staging.sellers
 
-SELECT TOP(10) *
-FROM staging.sellers
-    
-SELECT TOP(10) *
-FROM clean.customers
 
-SELECT TOP(10) *
-FROM clean.geolocation
+-- Add seller state full column
+ALTER TABLE clean.sellers
+ADD seller_state_full NVARCHAR(150)
+
+-- Update columns with geolocation state_full
+UPDATE c
+SET c.seller_state_full = g.geolocation_state_full
+FROM clean.sellers c
+JOIN clean.geolocation g
+    ON c.seller_state = g.geolocation_state
+
+
+-- Handle Nulls
+DECLARE @sql NVARCHAR(MAX)
+
+SELECT @sql = 
+    ' SELECT * 
+      FROM [clean].[sellers]
+      WHERE ' + STRING_AGG('[' + COLUMN_NAME + '] IS NULL', ' OR ')
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'sellers' AND TABLE_SCHEMA = 'clean'
+EXEC sp_executesql @sql
+
+-- No Nulls
+
+
+-- Handle Duplicates
+WITH Duplicates AS (
+    SELECT *, ROW_NUMBER () OVER (PARTITION BY seller_id, seller_zip_prefix, seller_city, seller_state, seller_state_full
+             ORDER BY (SELECT NULL)) AS occurance
+    FROM clean.sellers
+       
+)
+SELECT *
+FROM Duplicates
+WHERE occurance > 1
+-- No duplicates
+
+SELECT TOP(10) * 
+FROM clean.sellers
